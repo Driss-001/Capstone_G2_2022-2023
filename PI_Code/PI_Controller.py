974bae150853e9f9e45b090bd8fd5173623e4982
Driss-001 <drissmann001@gmail.com>
2023-01-16 01:40:20 -0500

    more placeholders

--- PI_Code/PI_Controller.py
+++ PI_Code/PI_Controller.py
@@ -14,7 +14,10 @@ import adafruit_ads1x15 as ADS
 path_length = 5 # in meters
 DAC_res =  12
 ADC_res =  16
+V_Max = 3.3 #max gpio output voltage
 
+#constant functions
+dac_raw = lambda volt: volt/V_Max*2**DAC_res
 #create I2C bus
 i2c = busio.I2C(board.SCL,board.SDA)
 dac = MCP.MCP4725(i2c)
@@ -24,18 +27,30 @@ class PI_Controller:
     
     #Initialisation
     def __init__(self,test = 0,continuous = 0,test_duration = 5,prototype = 0) -> None:
-        self.test = test 
+        self.test_status = test 
         self.continuous = continuous #if reading loops itself
-        self.prototype  = prototype # which prototype is being worked on 0:first,1:final
 
-        if self.test: #if test duration in minutes
-            self.test_duration = test_duration 
+        match self.test_status:
+            case 0: #PI simple signals
+                self._test = np.bool_([0,0]) 
+                self.test_duration = test_duration 
+            case 1: #Proto 1
+                self._test = np.bool_([0,1]) 
+                self.test_duration = test_duration 
+            case 2: #Proto 2
+                self._test = np.bool_([1,0]) 
+                self.test_duration = test_duration     
+            case 3: #Real case
+                self._test = np.bool_([1,1]) 
+                  
+            
 
     """Private functions in charge of hardware signal sending and collection"""
     #function governing LED activation, DAC
     def __LED(self):
 
-        if not self.prototype:
+        #no proto or proto 1
+        if not self._test[1]:
             pass
         else:
             pass
@@ -43,12 +58,26 @@ class PI_Controller:
 
     #function producing ramp signal to heat the u-chip, DAC
     def __Temp(self):
+
+        if not self._test[1]:
+            pass
+        else:
+            pass
         pass
+
 
     #function reading the photodiode output , ADC    
     def __PhotoDRead(self):
+        if not self._test[1]:
+            pass
+        else:
+            pass
         pass    
 
     #function storing initial T°=0 absorption throughport spectrum to compare with T°>0 spectrums 
     def _Calibrate(self):
+        if not self._test[1]:
+            pass
+        else:
+            pass
         pass
